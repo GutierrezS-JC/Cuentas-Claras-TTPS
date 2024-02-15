071b6e2b830e7b0a6b36e5551c7bffccca7b6674
Juan Cruz <juan.cruz.gutierrez.saravia@gmail.com>
2024-02-15 13:22:24 -0300

    Busqueda de usuarios general y eliminacion de lista de posibles invitados

--- frontend/src/app/components/modals/group/group-members/group-members.component.ts
+++ frontend/src/app/components/modals/group/group-members/group-members.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, Input } from '@angular/core';
+import { FormGroup, FormControl, FormsModule, ReactiveFormsModule, FormArray } from '@angular/forms';
 import { User } from '../../../../models/user/user.model';
+import { UserService } from '../../../../services/user/user.service';
 
 @Component({
   selector: 'app-group-members',
@@ -10,8 +11,14 @@ import { User } from '../../../../models/user/user.model';
   styleUrl: './group-members.component.css'
 })
 export class GroupMembersComponent {
+  constructor(
+    private userService: UserService,
+  ) { }
+  
+  @Input() user!: User;
 
   membersForm = new FormGroup({
+    selectedUserIds: new FormArray([]),
     searchInput: new FormControl('')
   })
 
@@ -24,5 +31,59 @@ export class GroupMembersComponent {
 
   setUserOption(option: string) {
     this.selectedUserOption = option;
+  }
+
+  // Busqueda de usuarios
+  handleSearch() {
+    let searchInput = this.membersForm.controls.searchInput.value as string;   
+    this.userService.searchUser(this.user.id, searchInput).subscribe({
+      next: (res: any) => {
+        this.searched = res;
+      },
+      error: (error) => {
+        console.log(error.message)
+      }
+    })
+  }
+
+  handleAddUser(user: User) {
+    const selectedUserIdsArray = this.membersForm.get('selectedUserIds') as FormArray;
+
+    if (selectedUserIdsArray.value.includes(user.id)) {
+      console.log('El usuario ya está en la lista');
+      return;
+    }
+
+    // Agrega el nuevo usuario al FormArray
+    selectedUserIdsArray.push(new FormControl(user.id));
+    this.selectedUsers = this.selectedUsers = [...this.selectedUsers, user];
+
+    // Limpiar input despues de agregar el usuario
+    this.membersForm.get('searchInput')?.reset('');
+    this.searched = [];
+  }
+
+  handleDeleteMember(member: User) {
+    const selectedUserIdsArray = this.membersForm.get('selectedUserIds') as FormArray;
+    const index = selectedUserIdsArray.value.indexOf(member.id);
+
+    if (index !== -1) {
+      selectedUserIdsArray.removeAt(index);
+    }
+    
+    this.selectedUsers = this.selectedUsers.filter(u => u.id !== member.id);
+  }
+
+
+  handleClose() {
+    this.membersForm.reset({
+      searchInput:''
+    })
+
+    const selectedUserIdsArray = this.membersForm.get('selectedUserIds') as FormArray;
+    selectedUserIdsArray.clear()
+
+    this.searched = [];
+    this.selectedUsers = [];
   }
 }
